5d8209c9307e7ee2bc00ebe470fa681e620b1d58
Will Sturgeon <willstrgn@gmail.com>
2024-12-22 01:16:59 -0800

    version up

--- flake.nix
+++ flake.nix
@@ -26,7 +26,7 @@
       system:
       let
         pname = "sigma-types";
-        version = "0.1.0";
+        version = "0.1.1";
         synopsis = "Types checked for an invariant.";
         description = synopsis;
         src = nix-filter {
